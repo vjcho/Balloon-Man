3c8fb24b2866621c18ed163a3e788d6f2c1c63fc
vjcho <vjcho@ucdavis.edu>
2016-01-30 16:37:19 -0800

    first commit

---
+++ Assets/Scripts/PlaneScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+    private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movement = new Vector2(speed.x*direction.x, speed.y * direction.y);
+	}
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+}
---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(2, 2);
+    public Vector2 direction = new Vector2(-1, 0);
+    public bool isLinkedToCamera = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = new Vector3(speed.x * direction.x, speed.y * direction.y, 0);
+
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+
+        if (isLinkedToCamera)
+        {
+            Camera.main.transform.Translate(movement);
+        }
+	}
+}
