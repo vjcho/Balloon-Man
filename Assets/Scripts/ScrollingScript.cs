bc6740dcc2f677c8694d05d6db8df223358fed09
vjcho <vjcho@ucdavis.edu>
2016-01-30 18:58:41 -0800

    made scenes folder

--- Assets/Scripts/ScrollingScript.cs
+++ Assets/Scripts/ScrollingScript.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ScrollingScript : MonoBehaviour {
 
     public Vector2 speed = new Vector2(2, 2);
     public Vector2 direction = new Vector2(-1, 0);
     public bool isLinkedToCamera = false;
+    bool isLooping = false;
+    private List<Transform> backgroundPart;
 
 	// Use this for initialization
 	void Start () {
-	
+        if (isLooping)
+        {
+            backgroundPart = new List<Transform>();
+            for(int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                if(child.GetComponent<Renderer>() != null)
+                {
+                    backgroundPart.Add(child);
+                }
+            }
+
+            //backgroundPart = backgroundPart.OrderBy(t => t.position.x).ToList();
+        }
 	}
 	
 	// Update is called once per frame
